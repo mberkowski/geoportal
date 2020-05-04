a145732c5abf39ed4f55f1f983bbbf89a39ff5f6
Michael Berkowski <mjb@umn.edu>
2020-05-04 15:55:23 -0500

    break a test on purpose

--- test/system/search_results_test.rb
+++ test/system/search_results_test.rb
@@ -23,7 +23,7 @@ class SearchResultsPageTest < ApplicationSystemTestCase
 
   def test_search
     visit '/?q=water'
-    assert page.has_content?("Search Results")
+    assert page.has_content?("Zearch Results")
   end
 
   def test_map_clustering
